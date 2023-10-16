df111a7467ff343e4a53d4e37a151f1c88d1b0fc
avtorklif <avtorklifavtorklif@gmail.com>
2023-10-16 23:29:18 +0300

    initial

---
+++ main.js
@@ -0,0 +1,16 @@
+const calcDPS = (dmg, rof) => 1 / (1 - rof / 100) * dmg * 100 - 100
+
+const handleInputs = () => {
+    const inputDmg = document.getElementById("inputDmgModifier").value;
+    const inputRof = document.getElementById("inputRateOfFireBonus").value;
+    const dpsBonus = calcDPS(parseFloat(inputDmg), parseFloat(inputRof));
+    const isValid = dpsBonus && dpsBonus < Number.POSITIVE_INFINITY && dpsBonus > 0;
+    const dpsBonusFmt = (Math.round(dpsBonus * 100) / 100).toFixed(2);
+    document.getElementById("pDpsBonus").innerText = isValid ? dpsBonusFmt : "?";
+}
+
+const initialize = () => {
+    document.getElementById("inputDmgModifier").value = 1.12;
+    document.getElementById("inputRateOfFireBonus").value = 11;
+    handleInputs();
+}
