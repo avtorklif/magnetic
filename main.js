6800442d1e6c6b928242b3ae04a29f9a202938bf
avtorklif <noreply@localhost>
2023-10-17 02:11:03 +0300

    user experience

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const calcDPS = (dmg, rof) => 1 / (1 - rof / 100) * dmg * 100 - 100
 
 const handleInputs = () => {
-    const inputDmg = document.getElementById("inputDmgModifier").value;
-    const inputRof = document.getElementById("inputRateOfFireBonus").value;
+    const inputDmg = document.getElementById("inputDmgModifier").value.replace(",", ".");
+    const inputRof = document.getElementById("inputRateOfFireBonus").value.replace(",", ".");
     const dpsBonus = calcDPS(parseFloat(inputDmg), parseFloat(inputRof));
     const isValid = dpsBonus && dpsBonus < Number.POSITIVE_INFINITY && dpsBonus > 0;
     const dpsBonusFmt = (Math.round(dpsBonus * 100) / 100).toFixed(2);
-    document.getElementById("pDpsBonus").innerText = isValid ? dpsBonusFmt : "?";
+    document.getElementById("pDpsBonus").innerText = isValid ? `${dpsBonusFmt}%` : "?";
 }
 
 const initialize = () => {
